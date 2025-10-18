10f90b7afa5cb16511ae34ae0948c0651a002ebe
topherx <topherx@gmail.com>
2025-10-18 13:44:26 +0100

    use ex_termbox2

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Ratatouille.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_termbox, "~> 1.0"},
+      {:ex_termbox2, "~> 0.1", github: "topherx/ex_termbox2", branch: "fix/compilation"},
       {:asciichart, "~> 1.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyze, "~> 0.2.0", only: :dev},
